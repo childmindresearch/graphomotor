1516fc816ab3032ad11b309c153b9355e9b47917
Alp Erkent <alp.erkent@gmail.com>
2025-04-30 13:53:44 +0000

    Refactor velocity metrics calculation to improve clarity and modularity, and update docstrings for better documentation. Also change pre-commit hook to reflect the swtich from `poetry` to `uv` for `toml` file formatting.

--- src/graphomotor/features/velocity.py
+++ src/graphomotor/features/velocity.py
@@ -6,45 +6,82 @@ from scipy import stats
 from graphomotor.core import models
 
 
+def _get_velocity_metrics(velocity: np.ndarray, type_: str) -> dict[str, float]:
+    """Calculate velocity metrics for a given type of velocity.
+
+    Args:
+        velocity: Numpy array of velocity values.
+        type_: Type of velocity (e.g., "linear_velocity", "radial_velocity",
+        "angular_velocity").
+
+    Returns:
+        Dictionary containing calculated metrics for the specified type of velocity.
+    """
+    return {
+        f"{type_}_sum": np.sum(np.abs(velocity)),
+        f"{type_}_variation": stats.variation(velocity),
+        f"{type_}_skewness": stats.skew(velocity),
+        f"{type_}_kurtosis": stats.kurtosis(velocity),
+    }
+
+
 def calculate_velocity_metrics(spiral: models.Spiral) -> dict[str, float]:
     """Calculate velocity-based metrics from spiral drawing data.
 
+    This function computes three types of velocity metrics by calculating the difference
+    between consecutive points in the spiral drawing data. The three types of velocity
+    are:
+        1. Linear velocity: The magnitude of change of Euclidean distance in pixels
+           per second. This is calculated as the square root of the sum of squares of
+           the differences in x and y coordinates divided by the difference in time.
+        2. Radial velocity: The magnitude of change of distance from center in pixels
+           per second. Radius is calculated as the square root of the sum of squares of
+           the x and y coordinates. The radial velocity is the change in radius divided
+           by the change in time.
+        3. Angular velocity: The magnitude of change of angle in radians per second.
+           Angle is calculated using the arctangent of y coordinates divided by x
+           coordinates. It is assumed that the drawing starts in the first quadrant
+           The angle is unwrapped to ensure continuity. The angular
+           velocity is the change in angle divided by the change in time.
+
+    For each velocity type, the following metrics are calculated:
+        - Sum: Sum of absolute velocity values
+        - Variation: Coefficient of variation
+        - Skewness: Asymmetry of the velocity distribution
+        - Kurtosis: Tailedness of the velocity distribution
+
     Args:
-        spiral: Spiral object containing drawing data
+        spiral: Spiral object containing drawing data.
 
     Returns:
-        Dictionary containing calculated velocity metrics
+        Dictionary containing calculated velocity metrics.
     """
-    x = spiral.data["x"].values - 50
-    y = spiral.data["y"].values - 50
-    t = spiral.data["seconds"].values
-    r = np.sqrt(x**2 + y**2)
-    theta = np.unwrap(np.arctan2(y, x))
+    x_coord = spiral.data["x"].values - 50
+    y_coord = spiral.data["y"].values - 50
+    time = spiral.data["seconds"].values
+    radius = np.sqrt(x_coord**2 + y_coord**2)
+    theta = np.unwrap(np.arctan2(y_coord, x_coord))
 
-    dx = np.diff(x)
-    dy = np.diff(y)
-    dt = np.diff(t)
-    dr = np.diff(r)
+    dx = np.diff(x_coord)
+    dy = np.diff(y_coord)
+    dt = np.diff(time)
+    dr = np.diff(radius)
     dtheta = np.diff(theta)
 
     vx = dx / dt
     vy = dy / dt
-    velocity = np.sqrt(vx**2 + vy**2)
+    linear_velocity = np.sqrt(vx**2 + vy**2)
 
     radial_velocity = dr / dt
     angular_velocity = dtheta / dt
 
+    linear_velocity_metrics = _get_velocity_metrics(linear_velocity, "linear_velocity")
+    radial_velocity_metrics = _get_velocity_metrics(radial_velocity, "radial_velocity")
+    angular_velocity_metrics = _get_velocity_metrics(
+        angular_velocity, "angular_velocity"
+    )
     return {
-        "velocity_sum": np.sum(np.abs(velocity)),
-        "velocity_cv": stats.variation(velocity),
-        "velocity_skewness": stats.skew(velocity),
-        "velocity_kurtosis": stats.kurtosis(velocity),
-        "radial_velocity_sum": np.sum(np.abs(radial_velocity)),
-        "radial_velocity_cv": stats.variation(radial_velocity),
-        "radial_velocity_skewness": stats.skew(radial_velocity),
-        "radial_velocity_kurtosis": stats.kurtosis(radial_velocity),
-        "angular_velocity_sum": np.sum(np.abs(angular_velocity)),
-        "angular_velocity_cv": stats.variation(angular_velocity),
-        "angular_velocity_skewness": stats.skew(angular_velocity),
-        "angular_velocity_kurtosis": stats.kurtosis(angular_velocity),
+        **linear_velocity_metrics,
+        **radial_velocity_metrics,
+        **angular_velocity_metrics,
     }
