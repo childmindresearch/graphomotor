8fdd123c4b3751eb41a1c095c667052a924af5f2
cgmaiorano <celiagrace13@gmail.com>
2025-12-09 10:00:20 -0500

    path optimality function

--- src/graphomotor/features/trails/drawing_metrics.py
+++ src/graphomotor/features/trails/drawing_metrics.py
@@ -1,5 +1,7 @@
 """Feature extraction module for drawing error-based metrics in trails drawing data."""
 
+import scipy.spatial.distance as dist
+
 from graphomotor.core import models
 
 
@@ -29,3 +31,31 @@ def get_total_errors(drawing: models.Drawing) -> dict[str, float]:
             "Drawing data does not contain 'total_number_of_errors' column."
         )
     return {"total_errors": drawing.data["total_number_of_errors"].iloc[0]}
+
+
+def calculate_path_optimality(
+    segment: models.LineSegment,
+    start_circle: models.CircleTarget,
+    end_circle: models.CircleTarget,
+) -> None:
+    """Calculate path optimality ratio.
+
+    Args:
+        segment: LineSegment object for which to calculate path optimality.
+        start_circle: CircleTarget representing the start circle.
+        end_circle: CircleTarget representing the end circle.
+
+    Returns:
+        Path optimality ratio.
+    """
+    optimal_distance = (
+        dist.euclidean(
+            [start_circle.cx, start_circle.cy], [end_circle.cx, end_circle.cy]
+        )
+        - start_circle.radius
+        - end_circle.radius
+    )
+
+    if optimal_distance > 0:
+        segment.path_optimality = optimal_distance / segment.distance
+    return
