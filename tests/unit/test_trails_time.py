eb317a803f299704af8f57442764255d6cebd512
cgmaiorano <celiagrace13@gmail.com>
2025-12-09 13:32:31 -0500

    ruff format

--- tests/unit/test_trails_time.py
+++ tests/unit/test_trails_time.py
@@ -8,10 +8,12 @@ from graphomotor.features.trails import time
 
 def test_total_error_time_no_errors() -> None:
     """Test case with no errors."""
-    df = pd.DataFrame({
-        "error": ["E0", "E0", "E0", "E0"],
-        "seconds": [0, 1, 2, 3],
-    })
+    df = pd.DataFrame(
+        {
+            "error": ["E0", "E0", "E0", "E0"],
+            "seconds": [0, 1, 2, 3],
+        }
+    )
     drawing = models.Drawing(data=df, task_name="trails", metadata={"id": "5555555"})
 
     result = time.calculate_total_error_time(drawing)
@@ -20,10 +22,12 @@ def test_total_error_time_no_errors() -> None:
 
 def test_single_error_chunk() -> None:
     """Test case with a single error chunk."""
-    df = pd.DataFrame({
-        "error": ["E0", "E1", "E1", "E0", "E0"],
-        "seconds": [0.0, 1.0, 3.0, 5.0, 6.0],
-    })
+    df = pd.DataFrame(
+        {
+            "error": ["E0", "E1", "E1", "E0", "E0"],
+            "seconds": [0.0, 1.0, 3.0, 5.0, 6.0],
+        }
+    )
     drawing = models.Drawing(data=df, task_name="trails", metadata={"id": "5555555"})
 
     result = time.calculate_total_error_time(drawing)
@@ -32,10 +36,12 @@ def test_single_error_chunk() -> None:
 
 def test_multiple_error_chunks() -> None:
     """Test case with multiple error chunks."""
-    df = pd.DataFrame({
-        "error": ["E0", "E1", "E1", "E0", "E2", "E2", "E0"],
-        "seconds": [0, 1, 3, 5, 6, 7, 9],
-    })
+    df = pd.DataFrame(
+        {
+            "error": ["E0", "E1", "E1", "E0", "E2", "E2", "E0"],
+            "seconds": [0, 1, 3, 5, 6, 7, 9],
+        }
+    )
     drawing = models.Drawing(data=df, task_name="trails", metadata={"id": "5555555"})
     result = time.calculate_total_error_time(drawing)
     assert result == {"total_error_time": 6.0}
@@ -43,10 +49,12 @@ def test_multiple_error_chunks() -> None:
 
 def test_error_at_end() -> None:
     """Test case with an error chunk that goes to the end of the drawing."""
-    df = pd.DataFrame({
-        "error": ["E0", "E0", "E2", "E2"],
-        "seconds": [0.0, 1.0, 2.0, 4.0],
-    })
+    df = pd.DataFrame(
+        {
+            "error": ["E0", "E0", "E2", "E2"],
+            "seconds": [0.0, 1.0, 2.0, 4.0],
+        }
+    )
     drawing = models.Drawing(data=df, task_name="trails", metadata={"id": "5555555"})
     result = time.calculate_total_error_time(drawing)
     assert result == {"total_error_time": 2.5}
@@ -54,10 +62,12 @@ def test_error_at_end() -> None:
 
 def test_error_at_start() -> None:
     """Test case with an error chunk that starts at the beginning of the drawing."""
-    df = pd.DataFrame({
-        "error": ["E1", "E1", "E0", "E0"],
-        "seconds": [0.0, 1.0, 3.0, 4.0],
-    })
+    df = pd.DataFrame(
+        {
+            "error": ["E1", "E1", "E0", "E0"],
+            "seconds": [0.0, 1.0, 3.0, 4.0],
+        }
+    )
     drawing = models.Drawing(data=df, task_name="trails", metadata={"id": "5555555"})
     result = time.calculate_total_error_time(drawing)
     assert result == {"total_error_time": 2.0}
