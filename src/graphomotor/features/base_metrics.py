71b902b002d9d7790d6212526e49d446f58c1519
cgmaiorano <celiagrace13@gmail.com>
2026-01-12 10:00:54 -0500

    move detect_pen_lifts into base metrics

--- src/graphomotor/features/base_metrics.py
+++ src/graphomotor/features/base_metrics.py
@@ -13,3 +13,15 @@ def get_task_duration(drawing: models.Drawing) -> dict[str, float]:
         Dictionary containing the total duration of the task in seconds.
     """
     return {"duration": drawing.data["seconds"].iloc[-1]}
+
+
+def detect_pen_lifts(drawing: models.Drawing) -> dict[str, int]:
+    """Detect pen lifts during a spiral drawing task.
+
+    Args:
+        drawing: Drawing object containing drawing data.
+
+    Returns:
+        Integer count of pen lifts detected.
+    """
+    return {"pen_lifts": len(drawing.data["line_number"].unique()) - 1}
