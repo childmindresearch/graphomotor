94f316b0f22aeb6455fb88dca57e21cf8c8317ee
cgmaiorano <celiagrace13@gmail.com>
2026-01-14 11:04:11 -0500

    move function into LineSegment class and move unit tests

--- src/graphomotor/features/trails/drawing_metrics.py
+++ src/graphomotor/features/trails/drawing_metrics.py
@@ -1,7 +1,5 @@
 """Feature extraction module for drawing error-based metrics in trails drawing data."""
 
-import scipy.spatial.distance as dist
-
 from graphomotor.core import models
 
 
@@ -31,38 +29,3 @@ def get_total_errors(drawing: models.Drawing) -> dict[str, float]:
             "Drawing data does not contain 'total_number_of_errors' column."
         )
     return {"total_errors": drawing.data["total_number_of_errors"].iloc[0]}
-
-
-def calculate_path_optimality(
-    segment: models.LineSegment,
-    start_circle: models.CircleTarget,
-    end_circle: models.CircleTarget,
-) -> None:
-    """Calculate path optimality ratio.
-
-    The default value for path optimality in the LineSegment object is 0.0. This
-    function updates the path_optimality attribute of the LineSegment object based on
-    the optimal distance between the start and end circles, adjusted for their radii.
-    If the optimal distance is less than or equal to zero, the path optimality remains
-    0.0.
-
-    Args:
-        segment: LineSegment object for which to calculate path optimality.
-        start_circle: CircleTarget representing the start circle.
-        end_circle: CircleTarget representing the end circle.
-
-    Returns:
-        Path optimality ratio.
-    """
-    optimal_distance = (
-        dist.euclidean(
-            [start_circle.center_x, start_circle.center_y],
-            [end_circle.center_x, end_circle.center_y],
-        )
-        - start_circle.radius
-        - end_circle.radius
-    )
-
-    if optimal_distance > 0:
-        segment.path_optimality = optimal_distance / segment.distance
-    return
